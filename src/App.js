7ff92c11196c3d0579a950d11e0f689e62b9bbe9
DavidWaza <wazabanga00@gmail.com>
2022-02-26 14:20:18 +0100

    api calls on selects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Axios from "axios";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -9,15 +9,16 @@ import Select from "@mui/material/Select";
 // import TableData from "./TableData";
 
 function App() {
-  const [age, setAge] = useState("");
-
+  const [age, setAge] = useState([]);
+  // const [data, setData] = useState("");
   const [students, setStudents] = useState([]);
+  const [state, setState] = useState("");
+  const [level, setLevel] = useState("");
+  const [gender, setGender] = useState("");
 
   useEffect(() => {
-    axios
-      .get("https://testapiomniswift.herokuapp.com/api/viewAllData")
+    Axios.get("https://testapiomniswift.herokuapp.com/api/viewAllData")
       .then((res) => {
-        console.log(res.data.data);
         setStudents(res.data.data.students);
       })
       .catch((err) => {
@@ -25,8 +26,33 @@ function App() {
       });
   }, []);
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  useEffect(() => {
+    handleAgeChange();
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  const handleAgeChange = () => {
+    Axios.get("https://testapiomniswift.herokuapp.com/api/viewAllAges")
+      .then((res) => {
+        console.log(res.data);
+        setAge(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleStateChange = (event) => {
+    setState(event.target.value);
+  };
+  const handleLevelChange = (event) => {
+    setLevel(event.target.value);
+  };
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
   };
 
   return (
@@ -36,7 +62,11 @@ function App() {
       </div>
       <div className="input-data">
         <p className="text">Filter Student Table By:</p>
-        <Box sx={{ maxWidth: 700 }} className="input-form">
+        <Box
+          sx={{ maxWidth: 700 }}
+          className="input-form"
+          onSubmit={submitHandler}
+        >
           <FormControl fullWidth className="form-control">
             <InputLabel id="demo-simple-select-label">Select Age</InputLabel>
             <Select
@@ -44,17 +74,24 @@ function App() {
               id="demo-simple-select"
               value={age}
               label="Select age"
-              onChange={handleChange}
-            ></Select>
+              onChange={handleAgeChange}
+            >
+              {age.map((ages) => {
+                return(
+                <MenuItem key={ages.id}>{ages.age}</MenuItem>
+                )
+              })}
+            </Select>
           </FormControl>
+
           <FormControl fullWidth className="form-control">
             <InputLabel id="demo-simple-select-label">Select State</InputLabel>
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              value={age}
+              value={state}
               label="Select State"
-              onChange={handleChange}
+              onChange={handleStateChange}
             ></Select>
           </FormControl>
           <FormControl fullWidth className="form-control">
@@ -62,9 +99,9 @@ function App() {
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              value={age}
+              value={level}
               label="Select Level"
-              onChange={handleChange}
+              onChange={handleLevelChange}
             ></Select>
           </FormControl>
           <FormControl fullWidth className="form-control">
@@ -72,9 +109,9 @@ function App() {
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              value={age}
+              value={gender}
               label="Select Gender"
-              onChange={handleChange}
+              onChange={handleGenderChange}
             >
               <MenuItem>Male</MenuItem>
               <MenuItem>Female</MenuItem>
@@ -85,37 +122,35 @@ function App() {
         </Box>
       </div>
       {/* table */}
-        <div className="api-data-table">
-          <table class="table">
-            <thead class="thead">
-              <tr>
-                <th scope="col">S/N</th>
-                <th scope="col">Surname</th>
-                <th scope="col">Firstname</th>
-                <th scope="col">Age</th>
-                <th scope="col">Gender</th>
-                <th scope="col">Level</th>
-                <th scope="col">State</th>
+      <div className="api-data-table">
+        <table className="table">
+          <thead className="thead">
+            <tr>
+              <th scope="col">S/N</th>
+              <th scope="col">Surname</th>
+              <th scope="col">Firstname</th>
+              <th scope="col">Age</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Level</th>
+              <th scope="col">State</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student) => (
+              <tr className="student-data" key={student.id}>
+                <th scope="row">{student.id}</th>
+                <td>{student.surname}</td>
+                <td>{student.firstname}</td>
+                <td>{student.age}</td>
+                <td>{student.gender}</td>
+                <td>{student.level}</td>
+                <td>{student.state}</td>
               </tr>
-            </thead>
-            <tbody>
-              {students.map((student) => (
-                <tr className="student-data">
-                  <th scope="row" key={student.id}>
-                    {student.id}
-                  </th>
-                  <td>{student.surname}</td>
-                  <td>{student.firstname}</td>
-                  <td>{student.age}</td>
-                  <td>{student.gender}</td>
-                  <td>{student.level}</td>
-                  <td>{student.state}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
+    </div>
   );
 }
 
